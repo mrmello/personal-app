dc95197b53956e6d1391edb3a13cdcfde5ff157e
mrmello <mrmello92@hotmail.com>
2017-06-10 11:20:53 -0300

    configuring html and ejs

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/airhorn/app'));
+app.set('views', __dirname + '/airhorn/app');
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
