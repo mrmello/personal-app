a6ea0f136369a8e4c68d66004731f8f2f94233f5
mrmello <mrmello92@hotmail.com>
2017-06-10 15:26:06 -0300

    recerting

--- index.js
+++ index.js
@@ -2,43 +2,15 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/airhorn/app'));
+app.set('views', __dirname + '/airhorn/app');
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('/airhorn/app/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+  response.render('/airhorn/app/index.html');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
 });
